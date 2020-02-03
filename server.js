048cc191044e5e8368b6d982664cb4eb3af1794a
qlba <qlba@ya.ru>
2020-02-03 16:18:14 +0300

    google

--- server.js
+++ server.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 var express = require('express');
 var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
+var Strategy = require('passport-google-oauth20').Strategy;
 
 
-// Configure the Facebook strategy for use by Passport.
+// Configure the Google strategy for use by Passport.
 //
 // OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
+// credential (`accessToken`) for accessing the Google API on the user's
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
 passport.use(new Strategy({
-    clientID: process.env['FACEBOOK_CLIENT_ID'],
-    clientSecret: process.env['FACEBOOK_CLIENT_SECRET'],
+    clientID: '1095237036957-qam61ccacseoupde2bj0a3ps4ou61peo.apps.googleusercontent.com',
+    clientSecret: 'ER8jiIuBdA56pw2Tfk9e8uOd',
     callbackURL: '/return'
   },
   function(accessToken, refreshToken, profile, cb) {
-    // In this example, the user's Facebook profile is supplied as the user
-    // record.  In a production-quality application, the Facebook profile should
+    // In this example, the user's Google profile is supplied as the user
+    // record.  In a production-quality application, the Google profile should
     // be associated with a user record in the application's database, which
     // allows for account linking and authentication with other identity
     // providers.
@@ -34,14 +34,22 @@ passport.use(new Strategy({
 // production-quality application, this would typically be as simple as
 // supplying the user ID when serializing, and querying the user record by ID
 // from the database when deserializing.  However, due to the fact that this
-// example does not have a database, the complete Facebook profile is serialized
+// example does not have a database, the complete Google profile is serialized
 // and deserialized.
+let USER = null;
+
 passport.serializeUser(function(user, cb) {
+	USER = user;
+
   cb(null, user);
 });
 
 passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+	console.log(' vvvv vvvv vvvv vvvv OUT obj vvvv vvvv vvvv vvvv');
+	console.dir(obj, {colors: true, depth: null});
+	console.log(' ^^^^ ^^^^ ^^^^ ^^^^ END obj ^^^^ ^^^^ ^^^^ ^^^^');
+
+  cb(null, USER);
 });
 
 
@@ -76,19 +84,27 @@ app.get('/login',
     res.render('login');
   });
 
-app.get('/login/facebook',
-  passport.authenticate('facebook'));
+app.get('/login/google',
+  passport.authenticate('google', { scope: ['profile'] }));
 
 app.get('/return', 
-  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
+	console.log(' vvvv vvvv vvvv vvvv OUT req.user vvvv vvvv vvvv vvvv');
+	console.dir(req.user, {colors: true, depth: null});
+	console.log(' ^^^^ ^^^^ ^^^^ ^^^^ END req.user ^^^^ ^^^^ ^^^^ ^^^^');
+
     res.redirect('/');
   });
 
 app.get('/profile',
   require('connect-ensure-login').ensureLoggedIn(),
   function(req, res){
+	console.log(' vvvv vvvv vvvv vvvv OUT req.user vvvv vvvv vvvv vvvv');
+	console.dir(req.user, {colors: true, depth: null});
+	console.log(' ^^^^ ^^^^ ^^^^ ^^^^ END req.user ^^^^ ^^^^ ^^^^ ^^^^');
+
     res.render('profile', { user: req.user });
   });
 
-app.listen(process.env['PORT'] || 8080);
+app.listen(process.env['PORT'] || 9999);
